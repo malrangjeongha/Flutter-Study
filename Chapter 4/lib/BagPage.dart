6fefd872ab8074f0756709907cf7e4be05521324
Jeongha <89580570+malrangjeongha@users.noreply.github.com>
2022-11-18 10:44:52 +0900

    Add files via upload

---
+++ Chapter 4/lib/BagPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BagPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Text('BagPage'),
+      ),
+    );
+  }
+}
